b79037a3fe933668e80c018c1ad3598a004925f0
Alpha-TesterMesser <computerkingang@gmail.com>
2025-04-02 16:50:16 -0700

    Added other options

--- Python Projects/Choose Adventure.py
+++ Python Projects/Choose Adventure.py
@@ -1,5 +1,20 @@
-def right_or_left():
+def go_in_or_not():
     while True:    
+        go_in_or_not = input("Would you like to go in the temple? ").lower()
+        if go_in_or_not == "yes":
+            print("You enter the temple, the darkness grows around you until you reach a t-junction")
+            print("")
+            right_or_left()
+        elif go_in_or_not == "no":
+            print("Um, okay coward...")
+            quit()
+        else:
+            print("Learn to type bozo!")
+            continue
+    else:
+        print("")
+
+def right_or_left(): 
         right_or_left = input("Would you like to go right or left? ").lower()
         if right_or_left == "right":
             print("You turn right...")
@@ -9,27 +24,7 @@ def right_or_left():
             left_turn()
         else:
             print("Learn to type bozo!")
-            continue
-
-def right_turn():
-    print("You see 3 levers and a sign: \n Two levers secure your safety, but one sends you to your doom...")
-    lever = input("Choose levers 1-3")
-    if lever.isdigit():
-        if lever == 1:
-            print("You are safe, you may continue...")
-            dungeon()
-        elif lever == 2:
-            print("You are safe, you may continue...")
-            dungeon()
-        elif lever == 3:
-            print("Stay strapped or get clapped, L Bozo!")
-            quit()
-    else:
-        print("Please enter a valid input!")
-
-def left_turn():
-    print("")
-    print("You turn left...")
+            right_or_left()
 
 def dungeon():
     print("")
@@ -39,4 +34,27 @@ def dungeon():
     if fight_or_run == "stand you ground":
         print("The monster seems taken aback at your boldness, and skids to a stop.")
 
-right_or_left()
+def right_turn():
+    print("You see 3 levers and a sign: \n Two levers secure your safety, but one sends you to your doom...")
+    lever = input("Choose levers 1-3 ")
+    if lever.isdigit():
+        if lever == 1:
+            print("You are safe, you may continue...")
+            dungeon()
+        elif lever == 2:
+            print("You are safe, you may continue...")
+            dungeon()
+        elif lever == 3:
+            print("Stay strapped or get clapped, L Bozo!")
+            quit(_ExitCode = None)
+    else:
+        print("Learn to type, tsk.. tsk.. tsk..")
+        right_turn
+
+def left_turn():
+    print("")
+    print("You turn left...")
+
+
+
+go_in_or_not()
