9e3a3f3880037cf2fe020c5bc61d91295208f07d
Alpha-TesterMesser <computerkingang@gmail.com>
2025-04-02 10:58:07 -0700

    Finish number guesser game

--- Python Projects/Number Guesser.py
+++ Python Projects/Number Guesser.py
@@ -1,32 +1,43 @@
-import random as rnd
+import random
 
 while True:
-  try:
-    num = int(input("Enter a number: "))
-    if num.isdigit():
-      if top_of_range > 1:
-        random_no = rnd.randint(1, top_of_range)
+    play = input("Would you like to play? (yes/no) ").lower()
+    if play == "yes":
+        print("Okay! Let's play.")
         break
-      else:
-        print("Please enter a valid number")
-    elif num == "exit":
-      quit()
+    elif play == "no":
+        print("Goodbye!")
+        quit()
     else:
-      print("Please enter a valid input!")
-      continue
-  except KeyError:
-    print("Please enter a valid input!")
-    
-guess_no += 1
-guess = input("Please enter your guess: ")
-if guess == random_no:
-  print("Well done, have a good day!")
-  quit()
-elif guess > random_no:
-  print("The number is larger than your guess!")
-elif guess < random_no:
-  print("The number is smaller than your guess!")
-else:
-  print("Please enter a valid number")
+        print("Please enter a valid input!")
+        continue
+
+while True:    
+    guess_no = 0
+    top_of_range = int(input("Please enter a number: "))
+    if top_of_range <= 1:
+        print("Please try again!")
+        continue
+    elif top_of_range > 1:
+        try:
+            top_of_range = int(top_of_range)
+            random_no = random.randint(1, top_of_range)
+            while True:
+                guess_no += 1
+                guess = int(input("Please enter your guess: "))
+                if guess == random_no:
+                    print("Well done! Let's play!")
+                    print(f"You got {random_no} in {guess_no} guesses!")
+                elif guess > random_no:
+                    print("The number is less than your guess!")
+                elif guess < random_no:
+                    print("The number is greater than your guess!")
+                else:
+                    print("How the heck?")
+                    continue
             
-print(guess_no)
+        except ValueError as e:
+            print("Please enter a valid input")
+    else:
+        print("Please enter a valid input!")
+        continue
